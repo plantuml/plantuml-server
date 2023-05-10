c663a446e4f525ff3edf47041e419cd4dfd31527
Florian <florian.heinrich@smail.th-koeln.de>
2023-05-10 21:14:57 +0200

    add emoji auto completion (suggestion)
    - typing `<:` will start the emoji auto complition inside the plantuml editor
    - for the sake of simplicity the emoji preview of the completion documentation will fetch the image from the original github repository (not plantuml). The reason is that the images (SVGs) inside plantuml have sometimes removed their svg tag, hence it's difficult to set the correct rendering size.
    - expand auto completion (suggestion) documentation by default
    - add emoji example GIF and documentation
    - set charset to utf-8 for each website
    - refactor JSON creation inside UI Helper

--- src/main/webapp/plantumllanguage.js
+++ src/main/webapp/plantumllanguage.js
@@ -284,6 +284,48 @@ const PlantUmlLanguageFeatures = (function() {
       });
     };
 
+    this.registerEmojiCompletion = () => {
+      const createEmojiProposals = async (range, filter = undefined) => {
+        const emojis = await this.getEmojis();
+        return emojis?.filter(([unicode, name]) => filter ? unicode.includes(filter) || name?.includes(filter) : true)
+          .map(([unicode, name]) => {
+            // NOTE: load images direct from GitHub source: https://github.com/twitter/twemoji#download
+            const emojiUrl = "https://raw.githubusercontent.com/twitter/twemoji/gh-pages/v/13.1.0/svg/" + unicode + ".svg";
+            const docHint = (name) ? name + " (" + unicode + ")" : unicode;
+            const isUnicode = !name || (filter && unicode.includes(filter));
+            const label = isUnicode ? unicode : name;
+            return {
+              label: label,
+              kind: monaco.languages.CompletionItemKind.Constant,
+              documentation: {
+                //supportHtml: true,  // also a possibility but quite limited html
+                value: "![emoji](" + emojiUrl + ") &nbsp; " + docHint
+              },
+              insertText: label + ":>",
+              range: range
+            };
+          }) || [];
+      };
+
+      monaco.languages.registerCompletionItemProvider(languageSelector, {
+        triggerCharacters: [":"],
+        provideCompletionItems: async (model, position) => {
+          const textUntilPosition = model.getValueInRange({
+            startLineNumber: position.lineNumber,
+            startColumn: 1,
+            endLineNumber: position.lineNumber,
+            endColumn: position.column,
+          });
+          const match = textUntilPosition.match(/<:(?<emoji>[^\s>]*)$/);
+          if (match) {
+            const suggestions = await createEmojiProposals(getWordRange(model, position), match.groups.emoji);
+            return { suggestions };
+          }
+          return { suggestions: [] };
+        }
+      });
+    };
+
 
     // ==========================================================================================================
     // == helper methods ==
@@ -305,6 +347,16 @@ const PlantUmlLanguageFeatures = (function() {
           icons = await makeRequest("GET", "ui-helper?request=icons", { responseType: "json" });
         }
         return icons;
+      }
+    })();
+
+    this.getEmojis = (function(){
+      let emojis = undefined;
+      return async () => {
+        if (emojis === undefined) {
+          emojis = await makeRequest("GET", "ui-helper?request=emojis", { responseType: "json" });
+        }
+        return emojis;
       }
     })();
 
@@ -377,6 +429,7 @@ const PlantUmlLanguageFeatures = (function() {
       this.addStartEndValidationListeners();
       this.registerThemeCompletion();
       this.registerIconCompletion();
+      this.registerEmojiCompletion();
     }
   }
 
